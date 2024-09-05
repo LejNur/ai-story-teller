be303450d1a58b2c959db3d81fcdc83d80c3b6dd
Lejla <lejla.nrk@gmail.com>
2024-09-05 09:38:45 +0200

    fix:flex-end

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,4 +1,18 @@
 export const genres = [
   { label: "Horror", value: "horror" },
   { label: "Romance", value: "romance" },
+  { label: "Action and Adventure", value: "action-adventure" },
+  { label: "Mystery", value: "mystery" },
+  { label: "Fantasy", value: "fantasy" },
+  { label: "Science Fiction", value: "sci-fi" },
+  { label: "Historical Fiction", value: "historical-fiction" },
+  { label: "Thriller", value: "thriller" },
+  { label: "Non-Fiction", value: "non-fiction" },
+  { label: "Biography", value: "biography" },
+  { label: "Self-Help", value: "self-help" },
+  { label: "Poetry", value: "poetry" },
+  { label: "Young Adult", value: "young-adult" },
+  { label: "Children's Literature", value: "children" },
+  { label: "Graphic Novel", value: "graphic-novel" },
+  { label: "Dystopian", value: "dystopian" },
 ];
